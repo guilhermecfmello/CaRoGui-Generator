b86e7fe15710e4551c6f8b9ebaebb6bbada81d6f
Guilherme <guilhermecfmello@gmail.com>
2019-11-19 10:20:34 -0300

    .

--- Ast.c
+++ Ast.c
@@ -8,21 +8,68 @@
 
 /*
 * struct types:
-*   - 'd': do-while
-*   - 'i': if
-*   - 'w': while
-*   - 'f': for
-*   - 'p': printf
-*   - 's': scanf
-*   - 'e': exit
-*   - 'r': return
+*   - 1: do-while
+*   - 2: if
+*   - 3: while
+*   - 4: for
+*   - 5: printf
+*   - 6: scanf
+*   - 7: exit
+*   - 8: return
 */
 
+
+/*
+* PLUS                9
+* MINUS              10
+* MULTIPLY           11
+* DIV                12
+* REMAINDER          13
+* BITWISE_AND        14
+* BITWISE_OR         15
+* BITWISE_XOR        16
+* LOGICAL_AND        17
+* LOGICAL_OR         18
+* EQUAL              19
+* NOT_EQUAL          20
+* LESS_THAN          21
+* GREATER_THAN       22
+* LESS_EQUAL         23
+* GREATER_EQUAL      24
+* R_SHIFT            25
+* L_SHIFT            26
+* ASSIGN             27
+* ADD_ASSIGN         28
+* MINUS_ASSIGN       29
+* VAR                30
+*/
+int nExp = 0;
+
 typedef struct __node {
-    char type;
+    int type;
     
-    Lista command_list;
+    Lista commands_then; // Command then used in while, if, for and do-while blocks
+    Lista commands_else; // Command else user in if block
+
+    Exp exp_start; // The start expression used on for block
+    Exp exp_stop; // Stop Condition
+    Exp exp_cond; // Conditional expression
+    Exp exp_adjust; // Values adjust of for block
+    Exp exp_end; // Expresion of addres used on pointers
+
+
+    char *string; // String value used on printf and scanf block
+
+
 } node;
+
+typedef struct _expression {
+    int type;
+    Exp left;
+    Exp right;
+
+    char *id; // Variable identifier
+} exp;
 
 typedef struct __ast {
     Lista nodes;
@@ -35,11 +82,61 @@ AST createAST(){
     return a;
 }
 
-Node addNode(AST as, char type){
-    node *n = (node*) malloc(sizeof(node));
-    ast *a = (ast*) a;
-    n->type = type;
-    insert(a, n);
+// Node addNode(AST as, int type){
+//     node *n = (node*) malloc(sizeof(node));
+//     ast *a = (ast*) as;
+//     n->type = type;
+//     insert(a, n);
+// }
 
+Exp expCreate(AST as,  int type){
+    ast *a = (ast*) as;
 
+    exp *e = (exp*) malloc(sizeof(exp));
+    e->type = type;
+    e->left = NULL;
+    e->right = NULL;
+    e->id = NULL;
 }
+
+Exp expSetId(Exp ex, char *identifier){
+    exp *e = (exp*) ex;
+
+    if(e->type == VAR)
+        e->id = identifier;
+    // else
+        // e->id = NULL;
+    return e;
+}
+
+Exp expInsertLeft(Exp parent, Exp child){
+    exp *e = (exp*) parent;
+    e->left = child;
+}
+
+
+Exp expInsertRight(Exp parent, Exp child){
+    exp *e = (exp*) parent;
+    e->right = child;
+}
+
+void printExpression(Exp ex){
+    exp *e = (exp*) ex;
+
+    if(ex->left != NULL){
+        nExp++;
+        printExpression(ex->left);
+    }
+    printf("LEFT level %d: %s", nExp, ex->id);
+    nExp = 0;
+    if(ex->right != NULL){
+        nExp++;
+        printExpression(ex->right);
+    }
+    printf("LEFT level %d: %s", nExp, ex->id);
+}
+
+
+
+
+
--- Ast.h
+++ Ast.h
@@ -5,15 +5,43 @@
 
 typedef void* AST;
 typedef void* Node;
+typedef void* Exp;
 
-#define DOWHILE    'd'
-#define IF         'i'
-#define WHILE      'w'
-#define FOR        'f'
-#define PRINTF     'p'
-#define SCANF      's'
-#define EXIT       'e'
-#define RETURN     'r'
+#define DOWHILE    1
+#define IF         2
+#define WHILE      3
+#define FOR        4
+#define PRINTF     5
+#define SCANF      6
+#define EXIT       7
+#define RETURN     8
+
+
+/* Expressions */
+#define PLUS                9
+#define MINUS              10
+#define MULTIPLY           11
+#define DIV                12
+#define REMAINDER          13
+#define BITWISE_AND        14
+#define BITWISE_OR         15
+#define BITWISE_XOR        16
+#define LOGICAL_AND        17
+#define LOGICAL_OR         18
+#define EQUAL              19
+#define NOT_EQUAL          20
+#define LESS_THAN          21
+#define GREATER_THAN       22
+#define LESS_EQUAL         23
+#define GREATER_EQUAL      24
+#define R_SHIFT            25
+#define L_SHIFT            26
+#define ASSIGN             27
+#define ADD_ASSIGN         28
+#define MINUS_ASSIGN       29
+#define VAR                30
+
+
 
 
 AST createAST();
@@ -29,11 +57,25 @@ AST createAST();
 *   - 'e': exit
 *   - 'r': return
 */
-Node addNode(AST a, char type);
+// Node addNode(AST a, int type);
+
+
 
 
 /* Print the AST tree on the default output */
 void printAST(AST a);
 
+/* createExpression(AST as,  int type); */
+Exp createExpression(AST as,  int type);
+
+Exp expSetId(Exp ex, char *identifier);
+
+Exp expInsertLeft(Exp parent, Exp child);
+
+
+Exp expInsertRight(Exp parent, Exp child);
+
+
+void printExpression(Exp ex);
 
 #endif
